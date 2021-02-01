49837d0182373d3ee40e3b90249eb6fbfece0a5c
Alex_Tomoiaga <atomoiaga@mogiletech.com>
2021-02-01 16:27:49 -0500

    Add new forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,9 +3,6 @@ const request = require('postman-request')
 const forecast = (lat, long, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=985a87dc1650d4f1ba82892919280a52&query=' + lat + ',' + long
-    //const url2 = 'http://api.weatherstack.com/current?access_key=985a87dc1650d4f1ba82892919280a52&query=37.8267,-122.4233&units=f'
-    //console.log(url)
-    //console.log(url2)
     request({ url, json:true}, (error, {body}) =>{
       if (error){
         callback('Unable to connect to forecast service', undefined)
@@ -16,7 +13,7 @@ const forecast = (lat, long, callback) => {
       }
       else
       {
-        const forecast = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+        const forecast = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is currently ' + body.current.humidity + '%.'
         callback(undefined, forecast)
       }
     })
